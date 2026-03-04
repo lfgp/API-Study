72c64e4f20899dcbbd4f8102abe3078e3cd7f0fb
Lucas Fonseca Gomes do Prado <48449255+lfgp@users.noreply.github.com>
2026-03-04 20:26:04 -0300

    Add files via upload

---
+++ storage/__init__.py
@@ -0,0 +1,8 @@
+from .jason_handler import JSONHandler, JasonHandler
+
+try:
+    from .cache_manager import CacheManager
+except ModuleNotFoundError:  # pragma: no cover - optional during partial installs
+    CacheManager = None
+
+__all__ = ["CacheManager", "JSONHandler", "JasonHandler"]
